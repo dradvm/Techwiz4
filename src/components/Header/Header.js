517227f361b7af2cf9bed75e2ca5e669a4f60f7e
Ndcas <ndhung9911@gmail.com>
2023-08-12 02:50:45 +0700

    fix bug cart, hoan thien checkout, chinh sua header doi mau

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,26 +3,53 @@ import { faBagShopping, faMagnifyingGlass } from "@fortawesome/free-solid-svg-ic
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import clsx from "clsx";
 import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import HeaderBg from "../HeaderBg/HeaderBg";
 import logo from "@images/logo.webp"
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { cartContext } from "../../App";
 function Header() {
+  const [search, setSearch] = useState('');
+  const [active, setActive] = useState('home');
   const importedCart = useContext(cartContext);
+  const navigate = useNavigate();
+  function searchEnter(e)
+  {
+    if (e.key === 'Enter')
+    {
+      goSearch();
+    }
+  }
+  function goSearch()
+  {
+    if (search.trim().length != 0)
+    {
+      getActive();
+      navigate('/search', {state: search});
+    }
+  }
+  function getActive()
+  {
+    setActive('');
+  }
+  function backHome()
+  {
+    navigate('/');
+    setActive('home');
+  }
   return (
     <>
       <HeaderBg/>
       <Container className="mt-1" fluid>
         <Row className={clsx("align-items-center", "border-bottom", "bottom-black", "px-5")}>
-          <Col ><div style={{width: "200px", cursor: "pointer"}} className="overflow-hidden"><img src={logo} alt= "logo-website" className="w-100 h-100"/></div></Col>
+          <Col ><div style={{width: "200px", cursor: "pointer"}} className="overflow-hidden" onClick={backHome}><img src={logo} alt= "logo-website" className="w-100 h-100"/></div></Col>
           <Col className={clsx("align-items-center")} >
             <div className={clsx(style["nav-bar"], "d-flex", "justify-content-between", "align-items-center")}>
-              <Link to = "/" className={clsx(style["nav-bar__item"], "text-decoration-none", style["nav-bar__item--home"])}>Home</Link>
-              <Link to = "/shop" className={clsx(style["nav-bar__item"], "text-decoration-none")}>Shopping</Link>
-              <Link to = "/contact" className={clsx(style["nav-bar__item"], "text-decoration-none")}>Contact Us</Link>
-              <Link to = "/feedback" className={clsx(style["nav-bar__item"], "text-decoration-none")}>Feedback</Link>
+              <Link to = "/" className={clsx(style["nav-bar__item"], "text-decoration-none", active == 'home' ? style["nav-bar__item--home"] : '')} onClick={() => setActive('home')}>Home</Link>
+              <Link to = "/shop" className={clsx(style["nav-bar__item"], "text-decoration-none", active == 'shopping' ? style["nav-bar__item--home"] : '')} onClick={() => setActive('shopping')}>Shopping</Link>
+              <Link to = "/contact" className={clsx(style["nav-bar__item"], "text-decoration-none", active == 'contact' ? style["nav-bar__item--home"] : '')} onClick={() => setActive('contact')}>Contact Us</Link>
+              <Link to = "/feedback" className={clsx(style["nav-bar__item"], "text-decoration-none", active == 'feedback' ? style["nav-bar__item--home"] : '')} onClick={() => setActive('feedback')}>Feedback</Link>
               
             </div>
           </Col>
@@ -32,13 +59,13 @@ function Header() {
               <div className={clsx(style["nav-bar__item"],style["icon"], "mx-3", "text-decoration-none", "position-relative")}>
                 <div className={clsx(style["input-search-box"], style["open"])}>
                   
-                  <input placeholder="Search..." aria-label="Search" aria-describedby="search"/>
-                  <div className={clsx(style["search"])}>
+                  <input placeholder="Search..." aria-label="Search" aria-describedby="search" value={search} onChange={(e) => setSearch(e.target.value)} onKeyDown={(e) => searchEnter(e)}/>
+                  <div className={clsx(style["search"])} onClick={goSearch}>
                     <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" className={clsx(style["icon"])}/>
                   </div>
                 </div>
               </div>
-              <div className={clsx(style["nav-bar__item"],style["icon"], style["shopping-cart-icon"], "ms-3", "position-relative", "z-3")}>
+              <div className={clsx(style["nav-bar__item"],style["icon"], style["shopping-cart-icon"], "ms-3", "position-relative", "z-3")} onClick={getActive}>
                 <Link  to = "/shopping-cart" className="text-decoration-none text-black"><FontAwesomeIcon icon={faBagShopping} size="lg" className="p-2"/></Link>
                 <div className={clsx(style["badge"], "position-absolute", "rounded-circle", "d-flex", "justify-content-center", "align-items-center")}>{importedCart.cart.length}</div>
                 <div  className={clsx(style["shopping-cart"])}>
@@ -52,5 +79,4 @@ function Header() {
     </>
   );
 }
-
-export default Header
+export default Header;
--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -4,8 +4,91 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { Col, Container, Row } from "react-bootstrap";
 import img from "@images/vertical-img.jpg"
-
+import { useLocation } from "react-router-dom";
+import { useRef, useState } from "react";
 function Checkout() {
+  const location = useLocation();
+  let sum = location.state;
+  const [email, setEmail] = useState('');
+  const [credit, setCredit] = useState('');
+  const [date, setDate] = useState('');
+  const [cvv, setcvv] = useState('');
+  let emailRef = useRef(null);
+  let creditRef = useRef(null);
+  let dateRef = useRef(null);
+  let cvvRef = useRef(null);
+  function emailEnter(e)
+  {
+    if (e.key === 'Enter')
+    {
+      creditRef.current.focus();
+    }
+  }
+  function creditEnter(e)
+  {
+    if (e.key === 'Enter')
+    {
+      dateRef.current.focus();
+    }
+  }
+  function dateEnter(e)
+  {
+    if (e.key === 'Enter')
+    {
+      cvvRef.current.focus();
+    }
+  }
+  function cvvEnter(e)
+  {
+    if (e.key === 'Enter')
+    {
+      pay();
+    }
+  }
+  function pay()
+  {
+    let regEx = /^[a-zA-Z]+[0-9a-zA-Z]*@[a-zA-Z0-9]+.[a-zA-Z]+$/;
+    if (!regEx.test(email))
+    {
+      alert('Please retype your email.');
+      emailRef.current.focus();
+      return;
+    }
+    regEx = /^[0-9]+$/;
+    if (!regEx.test(credit))
+    {
+      alert('Please retype your credit card number.');
+      creditRef.current.focus();
+      return;
+    }
+    if (date.trim().length === 0)
+    {
+      alert("Please retype your credit card's expire day.");
+      dateRef.current.focus();
+      return;
+    }
+    regEx = /^[0-9]{3}$/;
+    if (!regEx.test(cvv))
+    {
+      alert('Please retype your card verification value.');
+      cvvRef.current.focus();
+      return;
+    }
+    let pay =
+    {
+      email: email,
+      credit: credit,
+      expDate: date,
+      cvv: cvv,
+      value: sum
+    };
+    let payDetails = (JSON.parse(localStorage.getItem('payDetails')) || []);
+    localStorage.setItem('payDetails', JSON.stringify([...payDetails, pay]));
+    setEmail('');
+    setCredit('');
+    setDate('');
+    setcvv('');
+  }
   return (
     <Container className={clsx("my-5 p-4 bg-white d-flex align-items-center rounded", style["checkout-page"])} style={{ maxWidth: "900px" }}>
       <div className={clsx("w-100")}>
@@ -16,18 +99,18 @@ function Checkout() {
                 <h5 className="mb-3">Payment Details</h5>
 
                 <div className={clsx(style["input-group"], "d-flex flex-column mb-3")}>
-                  <input type="email" id="email-address" name="email-address" placeholder="Email Address..." />
+                  <input type="email" id="email-address" name="email-address" placeholder="Email Address..." ref={emailRef} value={email} onChange={(e) => setEmail(e.target.value)} onKeyDown={(e) => emailEnter(e)}/>
                 </div>
 
                 <div className={clsx(style["input-group"], "d-flex flex-column mb-3")}>
-                  <input type="email" id="email-address" name="email-address" placeholder="Credit Card Number..." />
+                  <input type="text" id="email-address" name="email-address" placeholder="Credit Card Number..." ref={creditRef} value={credit} onChange={(e) => setCredit(e.target.value)} onKeyDown={(e) => creditEnter(e)}/>
                 </div>
                 <div className="d-flex justify-content-between">
                   <div className={clsx(style["input-group"], "d-flex flex-column mb-3")} style={{ width: "40%" }} >
-                    <input type="email" id="email-address" name="email-address" placeholder="Expiry Date" />
+                    <input type="date" id="email-address" name="email-address" placeholder="Expiry Date" ref={dateRef} value={date} onChange={(e) => setDate(e.target.value)} onKeyDown={(e) => dateEnter(e)}/>
                   </div>
                   <div className={clsx(style["input-group"], "d-flex flex-column mb-3")} style={{ width: "40%" }} >
-                    <input type="email" id="email-address" name="email-address" placeholder="CVV" />
+                    <input type="text" id="email-address" name="email-address" placeholder="CVV" ref={cvvRef} value={cvv} onChange={(e) => setcvv(e.target.value)} onKeyDown={(e) => cvvEnter(e)}/>
                   </div>
                 </div>
               </div>
@@ -35,10 +118,10 @@ function Checkout() {
 
                 <div className="d-flex border-top pt-3 align-items-center justify-content-between mb-5">
                   <div className="fw-semibold text-secondary">Total Amount</div>
-                  <div className="fs-5 fw-semibold">500$</div>
+                  <div className="fs-5 fw-semibold">{'$' + sum}</div>
                 </div>
 
-                <button className={clsx(style["checkout"], "me-5")}>PAYMENT</button>
+                <button className={clsx(style["checkout"], "me-5")} onClick={pay}>PAYMENT</button>
               </div>
             </div>
           </Col>
